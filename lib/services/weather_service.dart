e134cfb43af138a6771cd423e386fa366a1ae4cc
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-10-21 21:08:41 +0300

    5. Handle HTTP Request & Response Exception using DioException

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -5,14 +5,23 @@ import '../models/weather.dart';
 class WeatherService {
   static const _baseUrl = 'http://api.weatherapi.com/v1';
   static const _apiKey = '5e6ed2285b65436a9f813120231408';
+  static const _defaultErrorMessage = 'Oops, there was an error, try later.';
 
   final Dio dio = Dio();
 
   Future<Weather> getWeather({required String cityName}) async {
-    final response = await dio.get(
-      '$_baseUrl/forecast.json?key=$_apiKey&q=$cityName&days=1',
-    );
+    try {
+      final response = await dio.get(
+        '$_baseUrl/forecast.json?key=$_apiKey&q=$cityName&days=1',
+      );
 
-    return Weather.fromMap(response.data);
+      return Weather.fromMap(response.data);
+    } on DioException catch (e) {
+      final String errorMessage =
+          e.response?.data['error']['message'] ?? _defaultErrorMessage;
+      throw Exception(errorMessage);
+    } catch (e) {
+      throw Exception(_defaultErrorMessage);
+    }
   }
 }
