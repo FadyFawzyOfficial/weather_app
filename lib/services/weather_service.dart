4c332a99a4bf52e969a698a781a8a40e52161be1
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-10-21 20:43:43 +0300

    4. Split the Base Url & API Key into Separated Variables

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -3,11 +3,14 @@ import 'package:dio/dio.dart';
 import '../models/weather.dart';
 
 class WeatherService {
+  static const _baseUrl = 'http://api.weatherapi.com/v1';
+  static const _apiKey = '5e6ed2285b65436a9f813120231408';
+
   final Dio dio = Dio();
 
   Future<Weather> getWeather({required String cityName}) async {
     final response = await dio.get(
-      'http://api.weatherapi.com/v1/forecast.json?key=5e6ed2285b65436a9f813120231408&q=$cityName&days=1',
+      '$_baseUrl/forecast.json?key=$_apiKey&q=$cityName&days=1',
     );
 
     return Weather.fromMap(response.data);
