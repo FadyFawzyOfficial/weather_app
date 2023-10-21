4de990f5c226eca55265c810883f0701a0a4a976
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-10-21 18:24:51 +0300

    3. Build SearchScreen & Navigate to it from HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../widgets/no_weather_body.dart';
+import 'search_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,6 +11,15 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Weather App'),
+        actions: [
+          IconButton(
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SearchScreen()),
+            ),
+            icon: const Icon(Icons.search_rounded),
+          ),
+        ],
       ),
       body: const NoWeatherBody(),
     );
